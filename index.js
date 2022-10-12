e80d015b1c9a1f786bf4a63035a39b23574c4e23
Andrew-McCall <amccall@qa.com>
2022-10-12 15:56:40 +0100

    Express Demoi

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-global.config = {...require(".config.json"), ...process.env}
+global.config = {...require("./.config.json"), ...process.env}
+const {Client, GatewayIntentBits} = require("discord.js")
+const express = require("express");
+const app = express();
 
-console.log("Hello World!")
+app.use(express.json())
+
+app.post("/message/:guild/:channel", (req, res)=> {
+    if (req.body.embeds || req.body.content){
+        global.client.guilds.fetch(req.params.guild).then(guild => {
+            guild.channels.fetch(req.params.channel).then(channel => {
+                channel.send(req.body).then((msg)=>{
+                    res.status(200).json(msg)
+                })
+            })
+        })
+    }else{
+        res.status(400).send("Invaild Body Format")
+    }
+})
+
+global.client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+global.client.on('ready', () => {
+	console.log(`Logged in as ${global.client.user.tag}!`)
+    app.listen(7777, ()=>{ // Express Started Here (AFTER discord)
+        console.log("Express started listening on 7777")
+    }) 
+});
+
+global.client.login(global.config.TOKEN);
